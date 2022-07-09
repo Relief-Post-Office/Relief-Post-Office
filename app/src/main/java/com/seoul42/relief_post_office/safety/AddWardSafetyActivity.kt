ec3062f8d5acd87b75a434e8db29f7296ebc02e6
신동해 <jeensh25@gmail.com>
2022-07-09 12:22:37 +0900

    exitbtn.png 추가, backbtn.png 추가, 피보호자 안부 추가 화면 추가

---
+++ app/src/main/java/com/seoul42/relief_post_office/safety/AddWardSafetyActivity.kt
@@ -0,0 +1,11 @@
+package com.seoul42.relief_post_office.safety
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddWardSafetyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_ward_safety)
+    }
+}
