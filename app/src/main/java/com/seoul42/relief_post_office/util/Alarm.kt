0b6d6ce941f721ecf4fca728c829b3d36da99cd8
moungjae joe <cmj092222@naver.com>
2022-07-10 13:25:54 +0900

    Design alarm UI

--- app/src/main/java/com/seoul42/relief_post_office/util/Alarm.kt
+++ app/src/main/java/com/seoul42/relief_post_office/util/Alarm.kt
@@ -1,15 +1,10 @@
 package com.seoul42.relief_post_office.util
 
-import android.app.AlarmManager
-import android.app.PendingIntent
 import android.content.Context
 import android.os.Build
 import android.os.PowerManager
-import java.util.concurrent.TimeUnit
 
 internal object Alarm {
-    const val INTERVAL_SECOND = 1
-
     /* 배터리 최적화 */
     fun isIgnoringBatteryOptimizations(context: Context): Boolean {
         val powerManager = context.getSystemService(Context.POWER_SERVICE) as PowerManager
