bbaefbe38333b978de78d84bec718404a0110bb9
moonheee <enduser0@naver.com>
2022-09-04 02:32:13 +0900

    (STYLE) add annotation - RPO-14

--- app/src/main/java/com/seoul42/relief_post_office/adapter/ResultAdapter.kt
+++ app/src/main/java/com/seoul42/relief_post_office/adapter/ResultAdapter.kt
@@ -13,7 +13,13 @@ import com.seoul42.relief_post_office.databinding.ItemResultBinding
 import com.seoul42.relief_post_office.model.ResultDTO
 import com.seoul42.relief_post_office.result.ResultDetailActivity
 import java.text.SimpleDateFormat
-
+/**
+ * 해당 날짜의 결과를 리사이클뷰에서 보여주기 위해 아이템에 값을 넣어줄 어뎁터 클래스
+ *
+ * - resultList : 연결된 노드는 모두 아이템으로 표시됩니다.
+ * - 표시할 내용 : 안부 이름, 안부 시작 시각, 응답 여부 또는 응답 하는 시간, 클릭 가능
+ * - 아이템 클릭 시 : 결과 상세 페이지로 넘어값니다. 미응답시 클릭되지 않습니다.
+*/
 class ResultAdapter(private val context : Context,
                     private val resultList: MutableList<Pair<String, ResultDTO>>,
                     private val wardId: String)
@@ -21,16 +27,19 @@ class ResultAdapter(private val context : Context,
     inner class ResultHolder(private val binding: ItemResultBinding) : RecyclerView.ViewHolder(binding.root){
         @SuppressLint("NotifyDataSetChanged", "ResourceAsColor")
         fun setResult(result: Pair<String, ResultDTO>) {
+            // 안부 이름 표시
             binding.itemResultSafetyName.text = result.second.safetyName
+            // 안부 시작 시간 표시
             binding.itemResultAlarmTime.text = result.second.safetyTime.replace(":", " : ")
-
+            // 응답 여부에 따라 응답 또는 응답 하는 시간, 클릭 가능, 클릭 리스너 표시 및 설정
             if (!isResponsed(result.second.responseTime)) {
-                setClickableResult(binding, result)
-            } else {
                 setUnClickableResult(binding, result)
+            } else {
+                setClickableResult(binding, result)
             }
         }
     }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ResultHolder {
         val binding = ItemResultBinding.inflate(LayoutInflater.from(parent.context),
             parent, false)
@@ -46,30 +55,55 @@ class ResultAdapter(private val context : Context,
         return resultList.size
     }
 
-    private fun setClickableResult(binding: ItemResultBinding, result: Pair<String, ResultDTO>) {
+    /**
+     * 안부에 응답을 했을 때 작동 하는 매서드
+     *
+     * 표시될 내용 : 응답 여부, 클릭 가능 여부(색깔)
+     * 아이템이 클릭 할 수 없습니다.
+     */
+    private fun setUnClickableResult(binding: ItemResultBinding, result: Pair<String, ResultDTO>) {
+        // 미응답 텍스트 표시
         binding.itemResultResponseTime.text = result.second.responseTime
-        binding.itemResultSafetyLayout.setBackgroundResource(R.drawable.result_disable_background)
         binding.itemResultResponseTime.setTextColor(R.color.red)
+        // 클릭 할 수 없는 색깔 표시
+        binding.itemResultSafetyLayout.setBackgroundResource(R.drawable.result_disable_background)
+        // 클릭 하면 간단한 토스트 메시지가 나오며 아무 일도 일어나지 않음
         binding.itemResultSafetyLayout.setOnClickListener {
             Toast.makeText(context, "미응답 결과입니다.", Toast.LENGTH_SHORT).show()
         }
     }
 
-    private fun setUnClickableResult(binding: ItemResultBinding, result: Pair<String, ResultDTO>) {
-        binding.itemResultSafetyLayout.setBackgroundResource(R.drawable.result_enable_background)
+    /**
+     * 안부에 응답하지 않았을 작동 하는 매서드
+     *
+     * 표시될 내용 : 응답 시간, 클릭 가능 여부(색깔)
+     * 아이템을 클릭 하면 결과 상세 페이지로 넘어갑니다.
+     */
+    private fun setClickableResult(binding: ItemResultBinding, result: Pair<String, ResultDTO>) {
+        // 응답 하는 시간 표시
         binding.itemResultResponseTime.text = millisToTimeString(calTimeDiff(result))
+        // 클릭 할 수 있는 색깔 표시
+        binding.itemResultSafetyLayout.setBackgroundResource(R.drawable.result_enable_background)
+        // 클릭 시 결과 상세 페이지로 넘어감
         binding.itemResultSafetyLayout.setOnClickListener { itemResultSafetyView ->
             // 여러번 클릭 방지
+            // 아이템 클릭 불가능
             itemResultSafetyView.isClickable = false
             val intent = Intent(context, ResultDetailActivity::class.java)
             intent.putExtra("wardId", wardId)
             intent.putExtra("resultId", result.first)
             intent.putExtra("result", result.second)
             ContextCompat.startActivity(context, intent, null)
+            // 아이템 클릭 가능
             itemResultSafetyView.isClickable = true
         }
     }
 
+    /**
+     * 응답 하는 시간 구하는 매서드
+     *
+     * - 응답 하는 시간(ms) = 결과 종료 시각 - 안부 설정 시작 시각
+     */
     private fun calTimeDiff(result: Pair<String, ResultDTO>): Long {
         val startTime = result.second.date + " " + result.second.safetyTime + ":00"
         val endTime = result.second.responseTime
@@ -77,6 +111,9 @@ class ResultAdapter(private val context : Context,
         return dateFormat.parse(endTime).time - dateFormat.parse(startTime).time
     }
 
+    /**
+     * 밀리 세컨드를 시간, 분, 초로 바꾸는 메서드
+     */
     private fun millisToTimeString(millisTime: Long): String{
         val timeSec = millisTime / 1000
         val hour = timeSec / 3600
@@ -85,6 +122,9 @@ class ResultAdapter(private val context : Context,
         return "${hour}시간 ${minute}분 ${second}초"
     }
 
+    /**
+     * 응답을 했는 지 안했는지 확인하는 메서드드
+    */
     private fun isResponsed(responseTime: String): Boolean {
         return responseTime != "미응답"
     }
--- app/src/main/java/com/seoul42/relief_post_office/adapter/ResultDetailAdapter.kt
+++ app/src/main/java/com/seoul42/relief_post_office/adapter/ResultDetailAdapter.kt
@@ -14,6 +14,15 @@ import com.seoul42.relief_post_office.databinding.ItemResultDetailBinding
 import com.seoul42.relief_post_office.model.AnswerDTO
 import com.seoul42.relief_post_office.result.ResultQuestionDetailActivity
 
+/**
+ * 해당 결과의 답변들을 리사이클뷰에서 보여주기 위해 아이템에 값을 넣어줄 어뎁터 클래스
+ *
+ * - 결과 id에 해당하는 모든 답변을 아이템으로 표시합니다.
+ * - 단, 비밀 옵션이 켜져 있을 경우 질문의 생성자가 아니면 아이템으로 표시 하지 않습니다.
+ * - 표시할 내용 : 질문 내용, 답변, 음성 답변 녹음 미디어 버튼
+ * - 음성 답변 녹음 미디어 버튼 클릭 시 : 음성 답변 미디어가 재생되거나 초기화 됩니다.
+ * - 답변 아이템 클릭 시 : 답변 상세 페이지로 넘어 갑니다.
+ */
 class ResultDetailAdapter (private val context : Context,
                            private val answerList: MutableList<Pair<String, AnswerDTO>>,
                            private val safetyName: String,
@@ -22,17 +31,23 @@ class ResultDetailAdapter (private val context : Context,
     inner class ResultDetailHolder(private val binding: ItemResultDetailBinding) : RecyclerView.ViewHolder(binding.root){
         @SuppressLint("NotifyDataSetChanged", "ResourceAsColor")
         fun setQuestionAnswer(answer: Pair<String, AnswerDTO>) {
+            // 질문 내용 표시
             setQuestionText(binding, answer.second)
+            // 음성 답변 미디어 설정
             setAnswerRecord(binding, answer.second)
+            // 답변 표시
             setAnswerReply(binding, answer.second)
+            // 답변 아이템 클릭시 답변 상세 페이지로 넘김
             binding.textResultQuetion.setOnClickListener { textResultQuestionView ->
                 // 여러번 클릭 방지
+                // 답변 아이템 클릭 불가능
                 textResultQuestionView.isClickable = false
                 val intent = Intent(context, ResultQuestionDetailActivity::class.java)
                 intent.putExtra("safetyName", safetyName)
                 intent.putExtra("answer", answer)
                 intent.putExtra("answerDate", answerDate)
                 startActivity(context, intent, null)
+                // 답변 아이템 클릭 가능
                 textResultQuestionView.isClickable = true
             }
         }
